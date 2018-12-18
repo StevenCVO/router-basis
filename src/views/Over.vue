e179e535d15483e477a7e7f46468204f21bc33b4
CVO script015 <script015@SJCA.LAN>
2018-12-18 21:37:32 +0100

    Eerste commit van project

---
+++ src/views/Over.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="over">
+        <h1>Over ons</h1>
+        <article>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique fuga totam distinctio qui vero vel! Soluta obcaecati, harum, quas ab autem a eos laboriosam dolor doloremque repudiandae odio deleniti labore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, quaerat recusandae. Aliquid velit eum provident enim alias labore illo nesciunt molestiae quaerat, aut consequuntur temporibus. Facilis possimus sequi voluptatibus, cumque consequuntur quia, illum blanditiis quis id nesciunt assumenda neque. In dolor cupiditate reiciendis velit magnam cum voluptate, explicabo laboriosam distinctio quia fuga nihil blanditiis quos incidunt fugiat beatae, vel consectetur pariatur ipsam, sit a provident doloremque. Sapiente, perferendis? Illum rerum repudiandae tempore cumque mollitia dolores assumenda et, impedit ex enim voluptatum officiis, optio minima voluptates. Sint molestias illo atque, adipisci quaerat ratione corrupti officia incidunt vero alias exercitationem ut quidem!
+            </p>
+
+            <hr>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique fuga totam distinctio qui vero vel! Soluta obcaecati, harum, quas ab autem a eos laboriosam dolor doloremque repudiandae odio deleniti labore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit dignissimos nam minus facilis porro. Assumenda dolorum harum obcaecati quibusdam enim corrupti id possimus expedita dicta nostrum tempore cumque, fugiat tempora ipsa iste autem odit facilis tenetur! Corporis iste ad quas nihil natus dicta quod magnam tenetur doloremque eum voluptatibus similique, aliquam eveniet maiores, omnis quidem. Unde nobis repellat nam! Dolorem.
+            </p>
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.over {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 5rem;
+}
+
+article {
+    padding: 3rem;
+    /* Tekst uitvullen */
+    text-align: justify;
+    max-width: 1152px;
+    line-height: 1.6;
+}
+
+h1 {
+    font-size: 7rem;
+    color: #40B782;
+}
+
+hr {
+    margin: 2rem 0;
+}
+</style>
